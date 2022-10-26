0eaeea37256deb18afbfd3d01e3683ac630d4ae2
Rebeca <r.sanchezd.d@alumnos.urjc.es>
2022-10-26 10:34:01 +0100

    Ejercicio 1.1

--- ejercicio1_1.py
+++ ejercicio1_1.py
@@ -1,82 +1,52 @@
-# ledRGB.py
+
+    # ledRGB.py
 # Practica 2. Sensores y actuadores. URJC. Julio Vega
 # Codigo de ejemplo de uso del LED RGB
 
 import time, sys
 import RPi.GPIO as GPIO
 
+rojo = "rojo"
+azul = "azul"
+verde = "verde"
 
-GPIO.setmode (GPIO.BOARD)
+def encender(pin):
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(pin,GPIO.OUT)
+    GPIO.output(pin, GPIO.LOW)
+    input("Ejecutando hasta que se pulse una tecla")
+    apagar(pin)
+        
+def apagar(pin):
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(pin, GPIO.OUT)
+    GPIO.output(pin, GPIO.HIGH)
+    
 
-GPIO.setup (11, GPIO.OUT)
-GPIO.setup (13, GPIO.OUT)
-GPIO.setup (15, GPIO.OUT)
+def main():
+	PROGRAMA = True
+	try:
+		while(PROGRAMA):
+			print("Introduce un color: rojo, azul o verde")
+			pin = input()
+			
+			if pin == rojo:
+				pin  = 11
+				encender(pin)
+			elif pin == azul:
+				pin = 13
+				encender(pin)
+			elif pin == verde:
+				pin = 15
+				encender(pin)
+			else:
+				print("Error")	
+								
+			
+	except KeyboardInterrupt:
+		PROGRAMA = False
+		GPIO.cleanup()
 
-
-rojo = GPIO.PWM (11, 100)
-azul = GPIO.PWM (13, 100)
-verde = GPIO.PWM (15, 100)
-
-
-print("Introduce un color")
-
-rojo.ChangeDutyCycle (1)
-azul.ChangeDutyCycle (1)
-verde.ChangeDutyCycle (1)
-
-pin = input()
-azuul = "azul"
-rojou = "rojo"
-verdeu = "verde"
-morado = "morado"
-blanco = "blanco"
-amarillo = "amarillo"
-cyan = "cyan"
-
-if pin == azuul:
-	rojo.ChangeDutyCycle(0)
-	input("Ejecutando hasta que se pulse una tecla")
-	GPIO.cleanup()
-	
-elif pin == rojou:
-	encender(rojoPin)
-	input("Ejecutando hasta que se pulse una tecla")
-	GPIO.cleanup()
-	
-elif pin == verdeu:
-	encender(verdePin)
-	input("Ejecutando hasta que se pulse una tecla")
-	GPIO.cleanup()
-	
-elif pin == morado:
-	encender(azulPin)
-	encender(rojoPin)
-	input("Ejecutando hasta que se pulse una tecla")
-	GPIO.cleanup()
-	
-elif pin == blanco:
-	encender(azulPin)
-	encender(rojoPin)
-	encender(verdePin)
-	input("Ejecutando hasta que se pulse una tecla")
-	GPIO.cleanup()
-	
-elif pin == amarillo:
-	encender(verdePin)
-	encender(rojoPin)
-	input("Ejecutando hasta que se pulse una tecla")
-	GPIO.cleanup()
-	
-elif pin == cyan:
-	encender(azulPin)
-	encender(verdePin)
-	input("Ejecutando hasta que se pulse una tecla")
-	GPIO.cleanup()
-	
-else:
-	print("Error")
-	
-
-
-
+if __name__ == "__main__":
+	main()	
     
